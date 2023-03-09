5d3e67889f481a3f99556238d02d3442f60bbdc1
Edita Krizmanic <ekrizmanic01@gmail.com>
2023-03-09 02:01:56 +0100

    added +prompt to years

--- src/components/pozdravImePrezimeGodina.jsx
+++ src/components/pozdravImePrezimeGodina.jsx
@@ -1,18 +1,18 @@
 const PozdravImePrezime = () => {
-  const imePrezime = prompt("Unesite svoje ime i prezime");
-  const godine = prompt("Koliko imate godina?");
+  const imePrezime = prompt("Unesite svoje ime i prezime")
+  const godine = +prompt("Koliko imate godina?");
   const PozdravImePrezime = imePrezime ? 
-    <div>
-      <h1>Pozdrav</h1>
-      <h2>korisnik</h2>
-      <h3>{imePrezime} ima {godine} godina</h3>
-    </div>
-   : 
-    <div>
-      <h1>Sorry.</h1>
-      <h2>Ne radimo s falsy vrijednostima.</h2>
-    </div>
-  
+  <div>
+    <h1>Pozdrav</h1>
+    <h2>korisnik</h2>
+    <h3>{imePrezime} ima {godine} godina</h3>
+  </div>
+  : 
+  <div>
+    <h1>Sorry.</h1>
+    <h2>Ne radimo s falsy vrijednostima.</h2>
+  </div>
+
 
   return (
     <div>
