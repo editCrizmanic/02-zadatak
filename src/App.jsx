9d84a947539dc5ebd7ff5b4a2629b0a1fbfd95fa
Edita Krizmanic <ekrizmanic01@gmail.com>
2023-03-09 01:21:44 +0100

    prompti idu dvaput i ne lovi falsy

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import { PozdravImePrezime } from "./components/pozdravImePrezimeGodina"
 
 function App() {
   return (
     <div className="App">
-      <h1>Zadatak s 2. predavanja</h1>
+      <p>Zadatak s 2. predavanja</p>
+      <PozdravImePrezime />
     </div>
   );
 }
